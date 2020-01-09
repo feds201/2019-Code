571c17b13f0833494327cb9fa02b778bd5f65963
KaranArora1 <karanarora2001@gmail.com>
2020-01-08 17:04:41 -0800

    howdy

--- src/main/include/vision.h
+++ src/main/include/vision.h
@@ -6,26 +6,16 @@
 /*----------------------------------------------------------------------------*/
 
 #pragma once
-
 #include "frc/smartdashboard/Smartdashboard.h"
 #include "networktables/NetworkTable.h"
 #include "networktables/NetworkTableInstance.h"
-#include "Drivetrain.h"
 
-#ifndef SRC_VISION_H_
-#define SRC_VISION_H_
-
-#include "WPILib.h"
-#include"CANTalon.h"
-
-class vision{
-std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+class vision {
+ public:
+ std::shared_ptr<NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
 double targetOffsetAngle_Horizontal = table->GetNumber("tx",0.0);
 double targetOffsetAngle_Vertical = table->GetNumber("ty",0.0);
 double targetArea = table->GetNumber("ta",0.0);
 double targetSkew = table->GetNumber("ts",0.0);
-
- 
+  vision();
 };
-
-#endif SRC_VISION_H_;
